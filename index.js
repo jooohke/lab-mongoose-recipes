3ae01ec53c0553ae924ee1a6bdf635dcbcdef6ab
Johanna Angot <63612183+jooohke@users.noreply.github.com>
2021-07-21 14:24:50 +0200

    lab done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
@@ -20,8 +19,53 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // CREATE RECIPE
+    Recipe.create({
+      title: "Chicken Adobo",
+      level: "Amateur Chef",
+      ingredients: ['2 lbspork belly', '2 tbs garlic', '5 dried bay leaves', '4 tbs vinegar', '1/2 cup soy sauce', '1 tbs peppercorn', '2 cups water', 'salt'],
+      cuisine: 'Pinoy',
+      dishType: "main_course",
+      duration: 70,
+      creator: 'Joh',
+    })
+    .then((createdDocument) => {
+      console.log(createdDocument.title);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+    Recipe.insertMany(data)
+    .then((createdDocument) => {
+      createdDocument.forEach(item => console.log(item.title))
+
+      Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+        .then((updatedDocument) => {
+          // console.log(updatedDocument.title + ' duration has been updated to ' + updatedDocument.duration);
+          console.log("updated document !!! " + updatedDocument)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      
+        Recipe.findOneAndDelete({title: "Carrot Cake"})
+        .then(() => {
+          console.log("Document deleted!")
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  mongoose.connection.close(() =>  {
+    console.log("connection closed")
+  })
